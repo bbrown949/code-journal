ab53f279013e58a11788b4780d5e9ebc9dd3c574
Bryan Brown <bryanbrown949@gmail.com>
2023-04-12 16:04:30 +0000

    Additional CSS edits

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+const localStorageData = localStorage.getItem('javascript-local-storage');
+
+window.addEventListener('beforeunload', function (event) {
+  const journalJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', journalJSON);
+});
+if (localStorageData !== null) {
+  data = JSON.parse(localStorageData);
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-const $url = document.querySelector('.url');
-$url.addEventListener('input', function (event) {
+const $photoURL = document.querySelector('.url');
+const $imagePlaceholder = document.querySelector('.image-placeholder');
+const entryImagePlaceholder = 'images/placeholder-image-square.jpg';
 
+$photoURL.addEventListener('input', function (event) {
+  $imagePlaceholder.setAttribute('src', event.target.value);
+});
+
+const $entryForm = document.querySelector('form');
+$entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const singleData = {
+    title: event.target.elements.title.value,
+    comment: event.target.elements.comment.value,
+    url: event.target.elements.url.value,
+    entryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.unshift(singleData);
+  $imagePlaceholder.setAttribute('src', entryImagePlaceholder);
+  $entryForm.reset();
 });
